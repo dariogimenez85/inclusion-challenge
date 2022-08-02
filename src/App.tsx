aa2174ec172db32ec7c5151198863189b5150176
Dario Gimenez <dario.gimenez@vibragaming.com>
2022-08-02 19:13:55 -0300

    basic react app structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import data from './data/services.json';
+
+const App = () => {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default App
