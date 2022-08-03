9d486dda02c1d0e9996d93ebd46bcf90003ed1f0
Dario Gimenez <dario.gimenez@vibragaming.com>
2022-08-03 18:03:27 -0300

    Change cards view from list to grid

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,16 +2,18 @@ import useServiceStatus, { ApiStatusData } from "../hooks/useServiceStatus";
 
 const Card = (props: any) => {
     const apiName: string = props.data.name;
-    const delay:number = props.delay;
+    const delay: number = props.delay;
 
     const { data } = useServiceStatus(apiName, delay);
 
     return (
-        <div className='row mt-3 border-bottom'>
-            <div className='col col-3'>{apiName.toUpperCase()}</div>
-            <div className='col col-3'>{(data as ApiStatusData).hostname}</div>
-            <div className='col col-4'>{(data as ApiStatusData).time}</div>
-            <div className={`col col-2 text-light text-center bg-${(data as ApiStatusData).status}`}>{(data as ApiStatusData).message}</div>
+        <div className='col p-1'>
+            <div className="card border-0 h-100 shadow p-1 mb-3 rounded  text-center">
+                <div className='card-header'>{apiName.toUpperCase()}</div>
+                <div className={`text-light bg-${(data as ApiStatusData).status}`}>{(data as ApiStatusData).message}</div>
+                <div className=''>{(data as ApiStatusData).hostname} &nbsp;</div>
+                <div className=''>{(data as ApiStatusData).time} &nbsp;</div>
+            </div>
         </div>
     );
 
